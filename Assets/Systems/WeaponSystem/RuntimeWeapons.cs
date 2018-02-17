0fcca2ff9766ce57b0994148a0d58bca1737efaf
Selegans2 <miguel.mejia@uao.edu.co>
2018-02-17 15:55:10 -0500

    Sistema de cambio de armas agregado

---
+++ Assets/Systems/WeaponSystem/RuntimeWeapons.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Weapon/Runtime Weapons")]
+public class RuntimeWeapons : ScriptableObject {
+
+    [Header("List of weapons")]
+    public List<Weapon> weapons = new List<Weapon>();
+    [Space(5)]
+
+    [Header("Current Weapon")]
+    public Weapon cur_Weapon;
+    [Space(5)]
+    public int r_curAmmo;
+    public int r_curCarryng;
+    public GameObject m_instance;
+
+    public void Init()
+    {
+        weapons.Clear();
+    }
+
+    public void weaponToActual(Weapon w)
+    {
+        cur_Weapon = w;
+        m_instance = w.w_prefab;
+
+        r_curAmmo = w.w_Stats.cur_Ammo;
+        r_curCarryng = w.w_Stats.cur_Carryng;
+    }
+
+    public void UnequipWeapon()
+    {
+        if (m_instance) {
+            Destroy(m_instance);
+        }
+    }
+}
---
+++ Assets/Systems/WeaponSystem/Weapon.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Weapon/Weapon")]
+public class Weapon : ScriptableObject {
+
+    [Header("Info")]
+    public string w_Name;
+    public weaponType type;
+    [Space(5)]
+
+    public weaponStats w_Stats;
+    [Space(5)]
+
+    [Header("Models")]
+    public GameObject w_prefab;
+
+    [System.Serializable]
+    public class weaponStats
+    {
+        public int cur_Ammo;
+        public int cur_Carryng;
+        public int cur_damage;
+        public int mod_damage;
+        public int cur_spread;
+        public int FireRate;
+    }
+
+    void Awake()
+    {
+        Init();
+    }
+
+    public void Init()
+    {
+        w_Stats = new weaponStats();
+    }
+}
