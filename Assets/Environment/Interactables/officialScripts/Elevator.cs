b6537e1d75f92500e12fc4a8558b72a905553dd3
cesar <cesar0572011@hotmail.com>
2018-02-17 01:00:37 -0500

    Agregando Assets correspondientes a la base militar(environment

---
+++ Assets/Environment/Interactables/officialScripts/Elevator.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class Elevator : MonoBehaviour {
+
+
+    GameObject lift;
+    GameObject top;
+    GameObject bottom;
+    GameObject screen;
+    AudioSource au;
+
+    Renderer rd;
+    Texture txtup;
+    Texture txtdown;
+    bool startMoving;
+    bool liftEnable=false;
+    bool once=true;
+    float speed;
+    bool up=true;
+    // Use this for initialization
+    void Start() {
+        screen = GameObject.Find("screen");
+        lift = GameObject.Find("Platform");
+        top = GameObject.Find("top");
+        bottom = GameObject.Find("bottom");
+        speed = 0.56f * Time.deltaTime;
+        au = lift.GetComponent<AudioSource>();
+        txtup = Resources.Load("up") as Texture;
+        txtdown = Resources.Load("down") as Texture;
+        rd = screen.GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+        if (liftEnable)
+        {
+            if (up)
+            {
+                rd.material.mainTexture = txtup;
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    startMoving = true;
+                    if (once == true)
+                    {
+                        au.Play();
+                        once = false;
+                    }
+                   
+                }
+                if (lift.transform.position != top.transform.position && startMoving == true)
+                {
+                    
+                    lift.transform.position = Vector3.MoveTowards(lift.transform.position, top.transform.position, speed);
+                    if (lift.transform.position == top.transform.position && startMoving)
+                    {
+                        startMoving = false;
+                        up = !up;
+                        once = true;
+                    }
+                }
+            }
+
+
+            if (up == false)
+            {
+                rd.material.mainTexture = txtdown;
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    startMoving = true;
+                    if (once == true)
+                    {
+                        au.Play();
+                        once = false;
+                    }
+                }
+                if (lift.transform.position != bottom.transform.position && startMoving == true)
+                {
+
+                    lift.transform.position = Vector3.MoveTowards(lift.transform.position, bottom.transform.position, speed);
+                    if (lift.transform.position == bottom.transform.position && startMoving)
+                    {
+                        startMoving = false;
+                        up = !up;
+                        once = true;
+                    }
+                }
+
+            }
+        }
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("P1"))
+        {
+            liftEnable = true;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("P1"))
+        {
+            liftEnable = false;
+        }
+    }
+}
---
+++ Assets/Environment/Interactables/officialScripts/doorOpen2.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class doorOpen2 : MonoBehaviour {
+
+
+    GameObject doorL;
+    GameObject doorR;   
+    GameObject targetL;
+    GameObject targetR;
+    GameObject mid;
+
+    AudioSource au;
+    ParticleSystem  psLe;
+    ParticleSystem  psRe;
+    float distance1;
+    float distance2;
+    bool open = false;
+
+    void Start()
+    {
+        psLe = GameObject.Find("SteamL2").GetComponent<ParticleSystem>();
+        psRe = GameObject.Find("SteamR2").GetComponent<ParticleSystem>();
+        au= gameObject.GetComponent<AudioSource>();
+        doorL = GameObject.Find("DoorL2");
+        doorR = GameObject.Find("DoorR2");
+        targetL= GameObject.Find("refLeft2");
+        targetR = GameObject.Find("refRight2");
+        mid = GameObject.Find("middle2");
+    }
+
+    // Update is called once per frame
+    void Update()
+    { 
+        if (open)
+        {
+            doorL.transform.position = Vector3.MoveTowards(doorL.transform.position, targetL.transform.position, 0.05f);
+            doorR.transform.position = Vector3.MoveTowards(doorR.transform.position, targetR.transform.position, 0.05f);
+        }
+        if (open == false)
+        {
+            doorL.transform.position = Vector3.MoveTowards(doorL.transform.position, mid.transform.position, 0.05f);
+            doorR.transform.position = Vector3.MoveTowards(doorR.transform.position, mid.transform.position, 0.05f);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if (other.CompareTag("P1"))
+        {
+            Debug.Log("entro");
+            open = true;
+            var Le = psLe.emission;
+            var Re = psRe.emission;
+            Le.enabled = true;
+            Re.enabled = true;
+            psLe.Play();
+            psRe.Play();
+            au.Play();
+        }
+
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("P1"))
+        {
+            Debug.Log("salio");
+            open = false;
+           
+        }
+    }
+}
