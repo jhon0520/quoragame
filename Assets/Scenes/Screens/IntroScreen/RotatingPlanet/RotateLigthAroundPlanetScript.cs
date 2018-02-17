aaa2e2622a52227224324e25cda744f6c9bcab7f
Gisler Garces Vargas <ggarces@uao.edu.co>
2018-02-17 15:18:10 -0500

    Se adiciona pantalla de inicio del juego

---
+++ Assets/Scenes/Screens/IntroScreen/RotatingPlanet/RotateLigthAroundPlanetScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateLigthAroundPlanetScript : MonoBehaviour {
+	public GameObject aroundObject;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.RotateAround(aroundObject.transform.position, Vector3.forward, 30 * Time.deltaTime);
+	}
+}
---
+++ Assets/Scenes/Screens/IntroScreen/StartScreenController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartScreenController : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+		AudioSource audio = GetComponent<AudioSource>();
+		audio.PlayDelayed (100);
+		audio.loop = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
