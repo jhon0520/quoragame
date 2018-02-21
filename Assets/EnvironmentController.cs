e1d6e1f31b296c663c94bcb4d9f04e5634673d8d
Gisler Garces <gislersoft@hotmail.com>
2018-02-21 00:04:12 -0500

    Integrando Start Screen con la escena 1 agregando ejemplo dialogo

---
+++ Assets/EnvironmentController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnvironmentController : MonoBehaviour {
+
+	/**
+	 * Al iniciar la pantalla ejecute un audio infinitamente.
+	 */
+	void Start () {
+		AudioSource audio = GetComponent<AudioSource>();
+		audio.PlayDelayed (100); // Espere 100 milisegundos antes de iniciar.
+		audio.loop = true;
+	}
+
+}
