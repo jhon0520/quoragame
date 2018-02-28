e0476e2417041ce273ad3d09dbda083d123393bd
Gisler Garces <gislersoft@hotmail.com>
2018-02-28 00:51:02 -0500

    Se cambia el character controller por el rigid se modifica el rigid controller y se mejoran los efectos bajo el agua

--- Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/RigidbodyFirstPersonController.cs
+++ Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/RigidbodyFirstPersonController.cs
@@ -1,13 +1,22 @@
 using System;
 using UnityEngine;
 using UnityStandardAssets.CrossPlatformInput;
+using Random = UnityEngine.Random;
 
 namespace UnityStandardAssets.Characters.FirstPerson
 {
     [RequireComponent(typeof (Rigidbody))]
     [RequireComponent(typeof (CapsuleCollider))]
+	[RequireComponent(typeof (AudioSource))]
     public class RigidbodyFirstPersonController : MonoBehaviour
     {
+		[SerializeField] private AudioClip[] m_FootstepSounds;    // an array of footstep sounds that will be randomly selected from.
+		[SerializeField] private AudioClip m_JumpSound;           // the sound played when character leaves the ground.
+		[SerializeField] private AudioClip m_LandSound;           // the sound played when character touches back on ground.
+
+		private AudioSource m_AudioSource;
+		public bool underwater = false; // If true the footstep sounds are not played.
+
         [Serializable]
         public class MovementSettings
         {
@@ -122,6 +131,7 @@ namespace UnityStandardAssets.Characters.FirstPerson
         {
             m_RigidBody = GetComponent<Rigidbody>();
             m_Capsule = GetComponent<CapsuleCollider>();
+			m_AudioSource = GetComponent<AudioSource>();
             mouseLook.Init (transform, cam.transform);
         }
 
@@ -155,6 +165,7 @@ namespace UnityStandardAssets.Characters.FirstPerson
                     (movementSettings.CurrentTargetSpeed*movementSettings.CurrentTargetSpeed))
                 {
                     m_RigidBody.AddForce(desiredMove*SlopeMultiplier(), ForceMode.Impulse);
+					PlayFootStepAudio ();
                 }
             }
 
@@ -168,6 +179,7 @@ namespace UnityStandardAssets.Characters.FirstPerson
                     m_RigidBody.velocity = new Vector3(m_RigidBody.velocity.x, 0f, m_RigidBody.velocity.z);
                     m_RigidBody.AddForce(new Vector3(0f, movementSettings.JumpForce, 0f), ForceMode.Impulse);
                     m_Jumping = true;
+					PlayJumpSound();
                 }
 
                 if (!m_Jumping && Mathf.Abs(input.x) < float.Epsilon && Mathf.Abs(input.y) < float.Epsilon && m_RigidBody.velocity.magnitude < 1f)
@@ -185,6 +197,47 @@ namespace UnityStandardAssets.Characters.FirstPerson
             }
             m_Jump = false;
         }
+
+		private void PlayFootStepAudio()
+		{
+			if (underwater){
+				return;
+			}
+
+			if (!m_IsGrounded)
+			{
+				return;
+			}
+			if (m_AudioSource.isPlaying) {
+				return;
+			}
+			// pick & play a random footstep sound from the array,
+			// excluding sound at index 0
+			int n = Random.Range(1, m_FootstepSounds.Length);
+			m_AudioSource.clip = m_FootstepSounds[n];
+			m_AudioSource.PlayOneShot(m_AudioSource.clip);
+			// move picked sound to index 0 so it's not picked next time
+			m_FootstepSounds[n] = m_FootstepSounds[0];
+			m_FootstepSounds[0] = m_AudioSource.clip;
+		}
+
+		private void PlayJumpSound()
+		{
+			if (underwater){
+				return;
+			}
+			m_AudioSource.clip = m_JumpSound;
+			m_AudioSource.Play();
+		}
+
+		private void PlayLandingSound()
+		{
+			if (underwater){
+				return;
+			}
+			m_AudioSource.clip = m_LandSound;
+			m_AudioSource.Play();
+		}
 
 
         private float SlopeMultiplier()
@@ -259,6 +312,7 @@ namespace UnityStandardAssets.Characters.FirstPerson
             if (!m_PreviouslyGrounded && m_IsGrounded && m_Jumping)
             {
                 m_Jumping = false;
+				PlayLandingSound ();
             }
         }
     }
---
+++ Assets/Systems/WaterSystem/UnderWaterEffect.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+/**
+ * Cambia el color de la luz del sol cuando se esta bajo el agua, para dar un efecto underwater,
+ * tambien aplica efectos de sonido como ingreso al agua y el efecto the estar sumergido.
+ */
+public class UnderWaterEffect : MonoBehaviour {
+
+	// Public
+	public GameObject planoAgua;
+	public AudioClip splashSound;
+	public Color colorAgua = new Color (0.0f, 0.0f, 1.0f);// Default Blue.
+	public float intensidadDelSol = 10.0f;
+	public float fuerzaAgua = 10.0f;
+	public Light luzDeSol;
+	public GameObject refPlayer;
+	public float disminuirCapacidadesBajoElAgua = 0.5f;
+	public Color colorFog;
+	public float fogDensity = 0.01f;
+	public float fogDensityBajoAgua = 0.3f;
+
+	// Private
+	private bool playSplash = false;
+	private Color colorOriginalSol;
+	private float alturaAgua = 0.0f;
+	private AudioSource aguaCiclo;
+	private float intensidadOriginal = 0.0f;
+
+	// Valores del FPS Controller a ser disminuidos bajo el agua.
+	private float JumpForce = 0.0f;
+	private float JumpForceD = 0.0f;
+
+	private float BackwardSpeed = 0.0f;
+	private float BackwardSpeedD = 0.0f;
+
+	private float ForwardSpeed = 0.0f;
+	private float ForwardSpeedD = 0.0f;
+
+	private float RunMultiplier = 0.0f;
+	private float RunMultiplierD = 0.0f;
+
+	private float StrafeSpeed = 0.0f;
+	private float StrafeSpeedD = 0.0f;
+
+	private Material skyBoxOriginal;
+
+	void Start()
+	{
+		alturaAgua = planoAgua.transform.position.y;
+
+		// Recuerde el color original que tenia el sol antes de continuar.
+		this.colorOriginalSol = this.luzDeSol.color;
+		this.intensidadOriginal = this.luzDeSol.intensity;
+
+		this.skyBoxOriginal = RenderSettings.skybox;
+
+		this.JumpForce = refPlayer.GetComponent<RigidbodyFirstPersonController> ().movementSettings.JumpForce;
+		this.JumpForceD = this.JumpForce * this.disminuirCapacidadesBajoElAgua;
+
+		this.BackwardSpeed = refPlayer.GetComponent<RigidbodyFirstPersonController> ().movementSettings.BackwardSpeed;
+		this.BackwardSpeedD = this.BackwardSpeed * this.disminuirCapacidadesBajoElAgua;
+
+		this.ForwardSpeed = refPlayer.GetComponent<RigidbodyFirstPersonController> ().movementSettings.ForwardSpeed;
+		this.ForwardSpeedD = this.ForwardSpeed * this.disminuirCapacidadesBajoElAgua;
+
+		this.RunMultiplier = refPlayer.GetComponent<RigidbodyFirstPersonController> ().movementSettings.RunMultiplier;
+		this.RunMultiplierD = this.RunMultiplier * this.disminuirCapacidadesBajoElAgua;
+
+		this.StrafeSpeed = refPlayer.GetComponent<RigidbodyFirstPersonController> ().movementSettings.StrafeSpeed;
+		this.StrafeSpeedD = this.StrafeSpeed * this.disminuirCapacidadesBajoElAgua;
+
+		aguaCiclo = GetComponent<AudioSource>();
+		aguaCiclo.loop = true;
+	}
+
+	/**
+	 * Aplica los valores disminuidos en el porcentaje de disminucion dado.
+	 */
+	void reducirSalto() {
+		refPlayer.GetComponent<RigidbodyFirstPersonController> ().underwater = true;
+		refPlayer.GetComponent<RigidbodyFirstPersonController> ().movementSettings.JumpForce = this.JumpForceD;
+		refPlayer.GetComponent<RigidbodyFirstPersonController> ().movementSettings.BackwardSpeed = this.BackwardSpeedD;
+		refPlayer.GetComponent<RigidbodyFirstPersonController> ().movementSettings.ForwardSpeed = this.ForwardSpeedD;
+		refPlayer.GetComponent<RigidbodyFirstPersonController> ().movementSettings.RunMultiplier = this.RunMultiplierD;
+		refPlayer.GetComponent<RigidbodyFirstPersonController> ().movementSettings.StrafeSpeed = this.StrafeSpeedD;
+	}
+
+	/**
+	 * Restaura los valores originales configurados para el FPS Controller. 
+	 */
+	void dejarSaltoOriginal() {
+		refPlayer.GetComponent<RigidbodyFirstPersonController> ().underwater = false;
+		refPlayer.GetComponent<RigidbodyFirstPersonController> ().movementSettings.JumpForce = this.JumpForce;
+		refPlayer.GetComponent<RigidbodyFirstPersonController> ().movementSettings.BackwardSpeed = this.BackwardSpeed;
+		refPlayer.GetComponent<RigidbodyFirstPersonController> ().movementSettings.ForwardSpeed = this.ForwardSpeed;
+		refPlayer.GetComponent<RigidbodyFirstPersonController> ().movementSettings.RunMultiplier = this.RunMultiplier;
+		refPlayer.GetComponent<RigidbodyFirstPersonController> ().movementSettings.StrafeSpeed = this.StrafeSpeed;
+	}
+
+	// Metodo update
+	void Update()
+	{
+		if (Camera.main.transform.position.y < this.alturaAgua ) {
+			this.luzDeSol.color = this.colorAgua;
+			this.luzDeSol.intensity = intensidadDelSol;
+			Physics.gravity = new Vector3(0, -5.8f, 0);
+		
+			RenderSettings.fogColor = colorFog;
+			RenderSettings.fogDensity = fogDensityBajoAgua;
+			RenderSettings.skybox = null;
+
+			reducirSalto ();
+
+		} else {
+			this.luzDeSol.color = this.colorOriginalSol;
+			this.luzDeSol.intensity = this.intensidadOriginal;
+			Physics.gravity = new Vector3(0, -9.8f, 0);
+
+			RenderSettings.fogColor = Color.black;
+			RenderSettings.fogDensity = fogDensity;
+			RenderSettings.skybox = this.skyBoxOriginal;
+
+			dejarSaltoOriginal ();
+		}
+
+		if (Camera.main.transform.position.y < this.alturaAgua) {
+			//this.playerControllerRef.gravity = 4.0f;
+
+			if (this.playSplash == true) {
+				AudioSource.PlayClipAtPoint (splashSound,Camera.main.transform.position);
+
+				aguaCiclo.Play ();
+				//Debug.Log (Physics.gravity);
+
+				this.playSplash = false;
+			}
+
+			Rigidbody rigidBody = refPlayer.GetComponent<Rigidbody>();
+			rigidBody.AddForce(transform.up * fuerzaAgua);
+			//rigidBody.useGravity = false;
+			//https://answers.unity.com/questions/11754/how-to-make-water-swimmable.html
+
+		} else {
+			//this.playerControllerRef.gravity = 20.0f;// TODO: Se puede obtener la referencia original.
+			this.playSplash = true;
+			aguaCiclo.Stop ();
+		}
+
+	}
+
+}
